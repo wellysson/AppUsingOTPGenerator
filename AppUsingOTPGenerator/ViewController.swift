57915eab0443fc856a69d369bc4b146c0a7b97a2
wellysson <wellyssonva@live.com>
2020-11-30 17:45:23 -0300

    Ajustes utilização biblioteca OTP.

--- AppUsingOTPGenerator/ViewController.swift
+++ AppUsingOTPGenerator/ViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import SwiftyRSA
 
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var countdownLabel: UILabel!
@@ -18,72 +19,30 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let deviceID = UIDevice.current.identifierForVendor?.uuidString
+        print(deviceID!)
         
         do {
             let keyPair = try SwiftyRSA.generateRSAKeyPair(sizeInBits: 2048)
+            
             print(try keyPair.publicKey.pemString())
             print(try keyPair.privateKey.pemString())
             
             let text = "Teste texto criptografia"
             print("Texto Limpo: \(text)")
-            let enctypt = self.encryptText(publicKeyString: try keyPair.publicKey.pemString(), text: text)
-            print(enctypt)
-            print("Texto decriptado: \(self.getDecrypt(priateKeyString: try keyPair.privateKey.pemString(), base64Encoded: enctypt))")
-            
+            if let enctypt = OTPHelper.otp?.encryptText(publicKeyString: try keyPair.publicKey.pemString(), text: text) {
+                print(enctypt)
+                if let decryptText = OTPHelper.otp?.getDecrypt(privateKey: try keyPair.privateKey.pemString(), base64Encoded: enctypt) {
+                    print("Texto decriptado: \(decryptText)")
+                }
+            }
             
             print("OK")
         } catch {
         }
         
-        
-        
-        
         self.startOTPGenerator()
     }
-    
-    func encryptText(publicKeyString: String, text: String) -> String {
-        var base64String = ""
-        
-        do {
-            let publicKey = try PublicKey(pemEncoded: publicKeyString)
-            let clear = try ClearMessage(string: text, using: .utf8)
-            let encrypted = try clear.encrypted(with: publicKey, padding: .PKCS1)
-
-            // Then you can use:
-//            let data = encrypted.data.
-            base64String = encrypted.base64String
-        } catch {
-            print(error)
-        }
-        
-        return base64String
-    }
-    
-    func getDecrypt(priateKeyString: String, base64Encoded: String) -> String {
-        do {
-            let privateKey = priateKeyString
-            
-            let privateKeyObj = try PrivateKey(pemEncoded: privateKey)
-            let encrypted = try EncryptedMessage(base64Encoded: base64Encoded)
-            let clear = try encrypted.decrypted(with: privateKeyObj, padding: .PKCS1)
-
-            let string = try clear.string(encoding: .utf8)
-            
-            return string
-        } catch let error {
-            print("erro decrypt:" + error.localizedDescription)
-        }
-        
-        return ""
-    }
-    
-//    func sha256(data : Data) -> Data {
-//        var hash = [UInt8](repeating: 0,  count: Int(CC_SHA256_DIGEST_LENGTH))
-//        data.withUnsafeBytes {
-//            _ = CC_SHA256($0.baseAddress, CC_LONG(data.count), &hash)
-//        }
-//        return Data(hash)
-//    }
     
     private func startOTPGenerator() {
         OTPHelper.otp?.startActivation(completion: { [weak self] registerSuccess in
